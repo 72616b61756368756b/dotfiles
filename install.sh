0a0ccd78635c8fa8ac72b55caa1bf90d1880d0ca
Kirill Rakauchuk <kirill.rakauchuk@gmail.com>
2020-04-26 13:03:59 +0300

    Update common install script

--- install.sh
+++ install.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
 IFS=$'\n\t'
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-source $DIR/.files/system/echos.sh
 SOURCE="https://github.com/rakauchuk/dotfiles"
-TARBALL="$SOURCE/tarball/master"
-TARGET="$HOME/.dotfiles"
-TAR_CMD="tar -xzv -C "$TARGET" --strip-components=1 --exclude='{.gitignore}'"
-[[ -d "$TARGET" ]] && rm -rf $TARGET
+TARBALL="${SOURCE}/tarball/master"
+TARGET="${HOME}/.dotfiles"
+TAR_CMD="tar -xzv -C "${TARGET}" --strip-components=1 --exclude='{.gitignore}'"
+
 _exists() {
   type "$1" >/dev/null 2>&1
 }
+
+[[ -d "${TARGET}" ]] && rm -rf ${TARGET}
 if _exists "git"; then
-  CMD="git clone $SOURCE $TARGET"
+  CMD="git clone ${SOURCE} ${TARGET}"
 elif _exists "curl"; then
-  CMD="curl -#L $TARBALL | $TAR_CMD"
+  CMD="curl -L ${TARBALL} | ${TAR_CMD}"
 elif _exists "wget"; then
-  CMD="wget --no-check-certificate -O - $TARBALL | $TAR_CMD"
+  CMD="wget --no-check-certificate -O - ${TARBALL} | ${TAR_CMD}"
 fi
-if [[ -z "$CMD" ]]; then
-  error "Error occurs, aborting..."
-else
-  running "Installing..."
-  mkdir -p "$TARGET"
-  eval "$CMD"
-  print_result $? 'Done'
+if [[ ! -z "${CMD}" ]]; then
+  echo "Installing..."
+  mkdir -p "${TARGET}"
+  eval "${CMD}"
+  /bin/bash $DIR/scripts/setup.sh "${TARGET}"
+  echo 'Done'
+  exit 0
 fi
-running "Linking..."
-$DIR/.files/system/link.sh
+exit 1
