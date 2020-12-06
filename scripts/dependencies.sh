03af890caf1d69165f05c045a5530ac0fab5b2d7
Kirill Rakauchuk <kirill.rakauchuk@gmail.com>
2020-12-06 22:41:19 +0300

    Updates

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -14,9 +14,6 @@ if [[ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
-source ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/usr/local/share/zsh-syntax-highlighting/highlighters
 
 if type brew &>/dev/null; then
